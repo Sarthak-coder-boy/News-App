a3b55eff4de1782e075dc69352865adf23b8ef83
Sarthak Gupta <gauravsarthak19@gmail.com>
2023-02-24 15:56:33 +0530

    changing in news

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ()=> {
         <nav className=" fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand " to="/">
-              NewsMonkey
+              NewsLife
             </Link>
             <button
               className="navbar-toggler"
